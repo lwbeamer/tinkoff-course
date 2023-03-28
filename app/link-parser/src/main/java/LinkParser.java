c2ce783e3c0f734d7b6cab03178c5b6bed743e13
lwbeamer <olibabamimedovich@gmail.com>
2023-03-28 10:31:01 +0300

    Now it uses java.net.URL features

--- app/link-parser/src/main/java/LinkParser.java
+++ app/link-parser/src/main/java/LinkParser.java
@@ -3,11 +3,8 @@ import parser.GitHubParser;
 import parser.StackOverflowParser;
 import result.ParseResult;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-
 public class LinkParser {
-    public ParseResult parseUrl(String url) throws MalformedURLException, URISyntaxException {
+    public ParseResult parseUrl(String url) {
         AbstractParser parser1 = new GitHubParser(null);
         AbstractParser parser2 = new StackOverflowParser(parser1);
 
