d7bdc603207a3fdf2c4ce622586fe8471cce90e5
lwbeamer <olibabamimedovich@gmail.com>
2023-03-28 10:10:43 +0300

    No it uses java.net.URL features

--- app/link-parser/src/main/java/parser/StackOverflowParser.java
+++ app/link-parser/src/main/java/parser/StackOverflowParser.java
@@ -3,6 +3,8 @@ package parser;
 import result.ParseResult;
 import result.StackOverflowParseResult;
 
+import java.net.URL;
+
 public class StackOverflowParser extends AbstractParser {
     public StackOverflowParser(AbstractParser nextParser) {
         super(nextParser);
@@ -10,19 +12,18 @@ public class StackOverflowParser extends AbstractParser {
 
     @Override
     public ParseResult parseResult(String url) {
+        URL toParse = tweakUrl(url);
+        if (toParse == null) return null;
 
-        if (url == null) return null;
-        String toParse = tweakUrl(url);
-        String[] tokens = toParse.split("/");
 
-        if (tokens.length >= 1 && tokens[0].equals("stackoverflow.com")) {
-            if (tokens.length >= 2 && tokens[1].equals("questions")) {
-                if (tokens.length >= 3) {
-                    try {
-                        return new StackOverflowParseResult(Long.parseLong(tokens[2]));
-                    } catch (NumberFormatException e) {
-                        return null;
-                    }
+        if (toParse.getHost().equals("stackoverflow.com")) {
+            String[] tokens = toParse.getFile().substring(1).split("/");
+            if (tokens.length >= 2 && tokens[0].equals("questions")) {
+                try {
+                    return new StackOverflowParseResult(Long.parseLong(tokens[1]));
+                } catch (NumberFormatException e) {
+                    System.out.println("Incorrect question ID");
+                    return null;
                 }
             } else return null;
         }
