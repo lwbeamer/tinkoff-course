165614b12404a48b49b4fe0636eba8bc399940d5
lwbeamer <olibabamimedovich@gmail.com>
2023-04-04 18:57:28 +0300

    Added tests + some fixes

--- app/bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
+++ app/bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
@@ -16,25 +16,9 @@ public class BotApplication {
         var ctx = SpringApplication.run(BotApplication.class, args);
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
 
-
-        ScrapperClient client = ctx.getBean(ScrapperClient.class);
-
         Bot bot = ctx.getBean(Bot.class);
 
         bot.start();
-
-//        System.out.println(client.getLinks(3L));
-//        System.out.println(client.addLink(3L, new AddLinkRequest("http://someNewLink")));
-//        System.out.println(client.getLinks(3L));
-//        System.out.println(client.deleteLink(3L,new RemoveLinkRequest("http://someNewLink")));
-//        System.out.println(client.getLinks(3L));
-//
-//        client.registerChat(10L);
-//        client.addLink(10L, new AddLinkRequest("http://someNewLink"));
-//        System.out.println(client.getLinks(10L));
-//
-//        client.deleteChat(10L);
-//        System.out.println(client.getLinks(10L));
 
 
     }
