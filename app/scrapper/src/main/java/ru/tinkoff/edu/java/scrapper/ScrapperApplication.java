b9962bbd8fdce7cc096601f71a0325d9f44fc321
lwbeamer <olibabamimedovich@gmail.com>
2023-05-06 16:17:33 +0300

    test events in actions

--- app/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
+++ app/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/ScrapperApplication.java
@@ -23,11 +23,6 @@ public class ScrapperApplication {
         ApplicationConfig config = ctx.getBean(ApplicationConfig.class);
 
 
-//        ScrapperQueueProducer sender = ctx.getBean(ScrapperQueueProducer.class);
-//        for (int i = 1; i < 10; i++){
-//            sender.updateLink(new LinkUpdate(10L,"some url","рэбит робит чи как",new Long[]{488021427L}));
-//            Thread.sleep(1000);
-//        }
 
     }
 }
