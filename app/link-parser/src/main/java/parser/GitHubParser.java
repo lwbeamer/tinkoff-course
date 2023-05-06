c19748eaf5df63ff6f0d068343b3bb5b6a4100f5
lwbeamer <olibabamimedovich@gmail.com>
2023-05-06 21:47:31 +0300

    linter rules are met for link-parser

--- app/link-parser/src/main/java/parser/GitHubParser.java
+++ app/link-parser/src/main/java/parser/GitHubParser.java
@@ -1,12 +1,8 @@
 package parser;
 
+import java.net.URL;
 import result.GithubParseResult;
 import result.ParseResult;
-
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Arrays;
 
 public class GitHubParser extends AbstractParser {
     public GitHubParser(AbstractParser nextParser) {
@@ -14,19 +10,18 @@ public class GitHubParser extends AbstractParser {
     }
 
     @Override
-    public ParseResult parseResult(String url) {
-        URL toParse = tweakUrl(url);
-        if (toParse == null) return null;
+    public ParseResult parseResult(URL url) {
 
-        if (toParse.getHost().equals("github.com")) {
-            String[] tokens = toParse.getFile().substring(1).split("/");
+        if (url.getHost().equals("github.com")) {
+            String[] tokens = url.getFile().substring(1).split("/");
             if (tokens.length >= 2) {
                 return new GithubParseResult(tokens[0], tokens[1]);
-            } else return null;
+            }
         }
 
-
-        if (nextParser != null) return nextParser.parseResult(url);
+        if (nextParser != null) {
+            return nextParser.parseResult(url);
+        }
 
         return null;
     }
